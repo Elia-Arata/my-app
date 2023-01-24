23645c4ef510264bbbdac14f4d791fb57250415e
Elia Arata <elia93.arata@gmail.com>
2023-01-24 15:34:59 +0100

    Struttura base

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  
-  return <>
-  <div className='Container'>
-    <h1 className='Cocktail'>Cocktail</h1>
-    <input className='input'></input>
-    <div className='Centro'>
-    <div className='ContenitoreDeiQuadrati'>
-    <div  className='Quadrato' >
-    </div>
-    <div className='Quadrato'>
-    </div>
-    <div className='Quadrato'>
-    </div>
-    <div className='Quadrato'>
-    </div>
-      <div className='Quadrato'>
-      </div>
-      <div className='Quadrato'>
-      </div>
-      <div className='Quadrato'>
-      </div>
-      <div className='Quadrato'>
-      </div>
-        <div className='Quadrato'>
+  return (
+    <>
+      <div className="Container">
+        <h1 className="Cocktail">Cocktail</h1>
+        <input className="input" placeholder="Cerca..."></input>
+        <div className="Centro">
+          <div className="ContenitoreDeiQuadrati">
+            <div className="Quadrato"></div>
+            <div className="Quadrato"></div>
+            <div className="Quadrato"></div>
+            <div className="Quadrato"></div>
+            <div className="Quadrato"></div>
+            <div className="Quadrato"></div>
+            <div className="Quadrato"></div>
+            <div className="Quadrato"></div>
+            <div className="Quadrato"></div>
+            <div className="Quadrato"></div>
+            <div className="Quadrato"></div>
+            <div className="Quadrato"></div>
+          </div>
         </div>
-    </div>
-
-    </div>
-
-
-
-
-  </div> 
-  
-  </>
-
+      </div>
+    </>
+  );
 }
 
 export default App;
